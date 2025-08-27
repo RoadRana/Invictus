90e313888aead934fe61c53daa0864a14cbc42e8
Rana Adel <73795077+RoadRana@users.noreply.github.com>
2025-08-27 16:51:07 +0200

    removing  env files

--- src/config/api.js
+++ src/config/api.js
@@ -1,9 +1,8 @@
 const config = {
   baseURL:
-    import.meta.env.VITE_API_BASE_URL || "https://3b980835104d.ngrok-free.app/",
+    import.meta.env.VITE_API_BASE_URL ,
   token:
-    import.meta.env.VITE_API_TOKEN ||
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzU4NjQ2Mjc5LCJpYXQiOjE3NTYwNTQyNzksImp0aSI6ImRjOTc5ZjA2YzhhMTRmYzZiMWU1ODIxOThhMThkZjRiIiwidXNlcl9pZCI6NH0.MJV6D_vsBuYoZjceV4dYBimNBx9L9TB5Z_UAyDKsSFI",
+    import.meta.env.VITE_API_TOKEN 
 };
 
 // Remove trailing slash from baseURL for consistency
