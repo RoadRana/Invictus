17c8f21e9461afab5e011f4843041c3885b92a08
Rana Adel <73795077+RoadRana@users.noreply.github.com>
2025-08-27 17:01:55 +0200

    fixing bug

--- src/config/api.js
+++ src/config/api.js
@@ -5,9 +5,6 @@ const config = {
     import.meta.env.VITE_API_TOKEN 
 };
 
-// Remove trailing slash from baseURL for consistency
-if (config.baseURL.endsWith("/")) {
-  config.baseURL = config.baseURL.slice(0, -1);
-}
+
 
 export default config;
