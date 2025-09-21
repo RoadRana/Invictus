c4d39d83529a82e0b2076ca9602781f7c7ae82d4
Hossam Arafat <hossam.arafat1994@hotmail.com>
2025-09-21 13:21:53 +0200

    Update ES Linting config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,13 +12,10 @@ module.exports = {
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh"],
   rules: {
-    
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
     ],
-    rules: {
-      "react/prop-types": "off",
-    },
+    "react/prop-types": "off",
   },
 };
